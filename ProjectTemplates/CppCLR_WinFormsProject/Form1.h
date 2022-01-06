cb4e9b4683824f2e064b57e1a8fe3616225aa032
sunchen0813 <sunchen0813@gmail.com>
2022-01-06 23:10:52 +0800

    加入專案檔案。

---
+++ ProjectTemplates/CppCLR_WinFormsProject/Form1.h
@@ -0,0 +1,59 @@
+#pragma once
+
+namespace CppCLRWinformsProject {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for Form1
+	/// </summary>
+	public ref class Form1 : public System::Windows::Forms::Form
+	{
+	public:
+		Form1(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add constructor code here.
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up used resources.
+		/// </summary>
+		~Form1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for providing designer assistance.
+		/// The content of the method must not be changed with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = gcnew System::ComponentModel::Container();
+			this->Size = System::Drawing::Size(300,300);
+			this->Text = L"Form1";
+			this->Padding = System::Windows::Forms::Padding(0);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+		}
+#pragma endregion
+	};
+}
---
+++ ProjectTemplates/CppCLR_WinFormsProject/pch.h
@@ -0,0 +1,12 @@
+// pch.h: This is a precompiled header file.
+// The files listed below are compiled only once to improve build performance for future builds.
+// This also affects IntelliSense performance, including code completion and many code browsing features.
+// However, the files listed here will ALL be recompiled if at least one of them is updated between builds.
+// Do not add any files here that should be updated frequently, as this will reverse the performance benefit.
+
+#ifndef PCH_H
+#define PCH_H
+
+// Add headers here to be precompiled.
+
+#endif //PCH_H
